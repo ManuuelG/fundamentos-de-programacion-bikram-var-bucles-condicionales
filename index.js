dbdc29656c428b1d2a627cd7ff75432d213b997a
ManuuelG <manuelguerreron93@gmail.com>
2023-09-22 17:44:30 +0200

    avance js

---
+++ index.js
@@ -0,0 +1,17 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI*2;
+var booleanoAnd = booleano1==booleano2;
+var booleanoNot = !booleano1;
+var booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+var incrementarDesp = 2;
+var resultadoDesp = incrementarDesp++;
+var incrementarAntes = 2;
+var resultadoAntes = ++incrementarAntes;
+var contarHasta10_2 = 0;
+for (var contarHasta10_2 = 0; contarHasta10_2 !== 10; contarHasta10_2++) {
+    console.log(contarHasta10_2);
+}
+
